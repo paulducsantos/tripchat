a9c3669ee8099ef447726b4a6119922291e14352
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-25 22:52:52 -0400

    Added Create and find routes

---
+++ routes/routes.js
@@ -0,0 +1,80 @@
+/*==========================================
+  ADD MODULE REQUIRES
+==========================================*/
+
+
+/*==========================================
+  ROUTES TO ACCESS DB
+==========================================*/
+
+
+// ************** CREATE **************
+
+//ADD NEW ITINERARIES BY CITY
+app.post('/addItinerary', function(req,res){
+  var newItinerary = req.body;
+    Itinerary.create(newItinerary)
+    .then(function(result){
+      res.redirect('/?msg=New Itinerary Create!');
+    }).catch(function(err){
+      res.redirect('/msg=Something Went Wrong! Please Try Again');
+    });
+});
+
+//ADD NEW COMMENT
+app.post('/newComment', function(req,res){
+  var newComment = req.body;
+  //NOT SURE ON THIS
+  newComment.itinerary.id = req.itinerary.id
+    Comment.create(newComment)
+    .then(function(result){
+      res.redirect('/?msg=Comment Added!');
+    }).catch(function(err){
+      res.redirect('/msg=Something Went Wrong! Please Try Again');
+    });
+});
+
+
+
+
+
+
+
+// ************** READY **************
+
+//  DO WE NEED A RES.RENDER SINCE WE ARE USING ANGULAR?
+
+//FIND ALL ITINERARIES
+app.get('/allItineraries', function(req,res){
+  Itinerary.findAll({})
+  .then(function(result){
+    res.render('/', {result});
+  });
+});
+
+//FIND ALL ITINERARIES BY CITY
+app.get('/allItineraries:location', function(req,res){
+  Itinerary.findAll({
+    where:{
+      location: req.body.location
+    }
+  })
+  .then(function(result){
+    res.render('/', {result});
+  });
+});
+
+//FIND ALL ITINERARIES BY SPECIFIC USER
+app.get('/username', function(req,res){
+  Itinerary.findAll({
+    where:{
+      username: req.body.username
+    }
+  })
+  .then(function(result){
+    res.render('/', {result});
+  });
+});
+
+
+
