1e56a1e5e62b981d2c68ef38bd60cbaa058583ee
MrMendonez <dmendonez@gmail.com>
2016-03-26 13:18:15 -0400

    added basic html and js

---
+++ public/js/scripts.js
@@ -0,0 +1,10 @@
+// Scripts for Angular To Do List App
+$(document).ready(function() {
+
+  // Triggers modal launch
+  $('.modal-trigger').leanModal();
+
+  // Activate side-nav for mobile
+  $(".button-collapse").sideNav();
+
+}); //end of doc.ready
