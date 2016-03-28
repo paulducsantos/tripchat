57c8db8214e0542cc3f6a6a6ef72895a3ffb31a3
Paul Santos <paulmichael.a.santos@gmail.com>
2016-03-28 12:42:04 -0400

    got the models to have relations when split into different files

---
+++ models/User.js
@@ -0,0 +1,51 @@
+var bcrypt            = require('bcryptjs');//REQUIRE FOR THE 
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        len: [6,30]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true  
+      }
+    },
+    fname: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true  
+      }
+    },
+    lname: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true  
+      }
+    },
+  }, {
+    hooks: {
+      beforeCreate: function(input){
+        input.userPassword = bcrypt.hashSync(input.userPassword, 10);
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        User.hasMany(models.Comment)
+      }
+    }
+  });
+
+  return User;
+}
