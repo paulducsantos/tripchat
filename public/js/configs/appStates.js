5cd226068a37c16b0f9313ca4fec4792621e65dc
Paul Santos <paulmichael.a.santos@gmail.com>
2016-03-30 23:56:10 -0400

    separated the html files to have partials and inject them into the index.html template

--- public/js/configs/appStates.js
+++ public/js/configs/appStates.js
@@ -1,12 +1,17 @@
 angular.module('TripChat')
 .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-  $urlRouterProvider.otherwise('/home');
+  // $urlRouterProvider.otherwise('/home');
   
   $stateProvider
-    .state('dashboard', {
-      url: '/dashboard',
-      templateUrl: 'views/partials/dashboard.html'
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/partials/home-partial.html'
     })
+    // .state('dashboard', {
+    //   url: '/dashboard',
+    //   templateUrl: 'views/partials/dashboard.html'
+    // })
 
   $locationProvider.html5Mode(true);
+}]);
