159a2b9c4bb391ca6d8c637f5a7e391a6b25635c
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-04-03 20:51:29 -0400

    able to print itineraries

--- public/js/controllers/dashboard-controller.js
+++ public/js/controllers/dashboard-controller.js
@@ -1,21 +1,42 @@
 angular.module("TripChat")
 .controller('dashboardCtrl', ['$scope', '$http', function($scope, $http) {
-  // $scope.showItineraries = function(){
-  //   $http.get("/itineraries").then(function (response) {
-  //     $scope.intineraries = response.data
-  //    });
-  // };
 
-  // $scope.userItineraries = function(){
-  //   $http.get("/users/" + user.id + "/itineraries").then(function (response) {
-  //     $scope.itineraries = response.data
-  //    });
-  // };
+  $scope.getUserItineraries = function() {
+
+    console.log($scope.user.id);
+    $http.get('/api/itineraries?UserId=' + $scope.user.id)
+    .then(function(result) {
+      $scope.userItineraries = result.data;
+      console.log(result.data);
+    }, function(err) {
+      console.log(err)
+    });
+  }
+
+  $scope.getUserItineraries();
+
+//FOR SEARCH PARTIAL WHEN COMPLETED
+  // $scope.getItineraries = function() {
+
+  //     console.log($scope.user.id);
+  //     $http.get('/api/itineraries')
+  //     .then(function(result) {
+  //       $scope.allItineraries = result.data;
+  //       console.log(result.data);
+  //     }, function(err) {
+  //       console.log(err)
+  //     });
+  //   }
+  // $scope.getItineraries();
+
+
+
 
   $scope.addItinerary = function(){
     $http.post("/api/itineraries", {
       title:$scope.itinerary_name,
-      location: $scope.location
+      location: $scope.location,
+      UserId: $scope.user.id
     })
     .success(function (response) {
       console.log(response.data);
