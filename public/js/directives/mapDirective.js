b7a5288dd29d54dd418f75614355eec67c18ca75
Paul Santos <paulmichael.a.santos@gmail.com>
2016-04-13 21:09:43 -0400

    map directive almost complete

--- public/js/directives/mapDirective.js
+++ public/js/directives/mapDirective.js
@@ -1,6 +1,27 @@
 angular.module('TripChat')
-.directive('smallMap', ['$scope', '$http', function ($scope, $http) {
- 
+.directive('smallMap', function () {
+  return {
+    restrict: 'A',
+    scope: {
+      city: '='
+    },
+    templateUrl: '/views/directives/smallMap.html',
+    controller: function($scope, $http) {
+
+      $scope.map = {
+        center: {
+          latitude: 40.7128,
+          longitude: -74.0059
+        },
+        zoom: 12,
+        options: {
+          scrollwheel: false,
+          draggable: true
+        },
+        markers: []      
+      };
 
 
-}]);
+    }
+  }
+});
